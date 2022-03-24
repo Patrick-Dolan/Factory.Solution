d1ca795c9d48f889fd503766bbb1eae6b1d52a21
Patrick Dolan <dolanp1992@gmail.com>
2022-03-24 15:32:42 -0700

    add index view of engineers

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Factory.Models;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      return View(_db.Engineers.ToList());
+    }
+  }
+}
