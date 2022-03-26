d2294795b89b07ce8b270a4a905cbc68c9b33b2e
Patrick Dolan <dolanp1992@gmail.com>
2022-03-25 18:29:59 -0700

    add delete functionality to machine system

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -65,5 +65,20 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Details", new { id = machine.MachineId});
     }
+
+    public ActionResult Delete(int id)
+    {
+      Machine machine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      return View(machine);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Machine machine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      _db.Machines.Remove(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
